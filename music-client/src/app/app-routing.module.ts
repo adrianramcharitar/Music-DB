5defcd7cd2debdcf0420e036a56d0be6f2244fbd
Adrian Ramcharitar <adrian007760@gmail.com>
2021-02-08 20:40:37 -0500

    Added main page and routing

--- music-client/src/app/app-routing.module.ts
+++ music-client/src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ArtistPageComponent} from './components/artist-page/artist-page.component';
+import {SongPageComponent} from './components/song-page/song-page.component';
+import {AlbumPageComponent} from './components/album-page/album-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'artists', component: ArtistPageComponent },
+  { path: 'albums', component: AlbumPageComponent },
+  { path: 'songs', component: SongPageComponent },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- music-client/src/app/app.module.ts
+++ music-client/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -9,13 +8,15 @@ import { MaterialModule } from './material.module';
 import { ArtistPageComponent } from './components/artist-page/artist-page.component';
 import { AlbumPageComponent } from './components/album-page/album-page.component';
 import { SongPageComponent } from './components/song-page/song-page.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArtistPageComponent,
     AlbumPageComponent,
-    SongPageComponent
+    SongPageComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
